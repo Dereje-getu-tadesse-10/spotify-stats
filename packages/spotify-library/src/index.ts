f4faee073aaedf4bdc49b4c87da5643ffa6aab5b
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-03-16 20:02:25 +0100

    feat: create types package

--- packages/spotify-library/src/index.ts
+++
@@ -1,5 +0,0 @@
-const response = "It's me, hi, I'm the problem, it's me";
-
-export const antiHero = (): string => {
-  return response;
-};
---
+++ packages/spotify/src/index.ts
@@ -0,0 +1,5 @@
+import { AntiHero } from "@repo/types";
+
+const response = "It's me, hi, I'm the problem, it's me";
+
+export const antiHero: AntiHero = () => response;
