f56c7b517a5fce2732c964d823931bd92fbfa0d1
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-03-16 15:41:52 +0100

    initial commit

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page(): JSX.Element {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24 bg-red-500">
+      <h1>Hello</h1>
+    </main>
+  );
+}
